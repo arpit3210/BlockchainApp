d92c20b2349a06753b4dce2d8c889c0685c43b74
1234 <arpitsinghthakur321@gmail.com>
2024-01-04 01:16:08 +0530

    added few responsive in tailwindconfig file

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -44,9 +44,9 @@ const Welcome = () => {
   return (
     <div className='flex w-full justify-center items-center'>
 
-      <div className='flex md:flex-row flex-col items-start justify-between  md:p-20 py-12 px-4 '>
+      <div className='flex mf:flex-row flex-col items-start justify-between  md:p-20 py-12 px-4 '>
 
-        <div className='flex flex-1 justify-start  flex-col md:mr-10  '>
+        <div className='flex flex-1 justify-start  flex-col mf:mr-10  '>
           <h1 className='text-white sm:text-5xl text-gradient py-1 '>
             Send Crypto <br />  across teh world
           </h1>
@@ -98,7 +98,7 @@ const Welcome = () => {
 
 
 
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10 '>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10 '>
 
           <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism '>
             {/* csshero.org/mesher  */}
@@ -148,7 +148,7 @@ const Welcome = () => {
 
 
 
-            {true ? (
+            {false ? (
 
               <>
                <Loader></Loader>
